e721ebf07f7a21ee9af34b6ebe5c45a6daeabe18
Soumyadeep Chatterjee <KH9376@kitspl.com>
2019-03-12 14:32:57 +0530

    Adds Map Component

---
+++ src/app/common/map/map.component.ts
@@ -0,0 +1,31 @@
+import { Component, Input, ChangeDetectorRef } from '@angular/core';
+import { MapService } from './map.service';
+
+
+@Component({
+  selector: 'app-map',
+  templateUrl: './map.component.html',
+  styleUrls: ['./map.component.scss']
+})
+export class MapComponent {
+
+  @Input() location : string;
+
+  isPositionError: boolean = false;
+  lat: number;
+  lng: number;
+  constructor(private mapService : MapService,
+              private ref : ChangeDetectorRef) { }
+
+  mapReadyFunctionCall(){
+    this.mapService.geocodelocation(this.location).subscribe((coords)=>{
+      this.lat = coords.lat;
+      this.lng = coords.lng;
+
+      this.ref.detectChanges();
+    }, () => {
+      this.isPositionError = true;
+    });
+  }
+
+}
